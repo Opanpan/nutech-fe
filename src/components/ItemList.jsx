843d23528b78d5646fd3c1579f88134dcc711fa0
Opanpan <fanalriansyah@gmail.com>
2021-12-19 03:06:00 +0700

    create handle to add item, and get data item from user

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,19 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Axios from "axios";
 import Item from "./Item";
 import ItemForm from "./ItemForm";
 
 export default function ItemList(props) {
-  // HANDLE ADD EMPLOYEE
+  const [items, setItems] = useState([]);
+
+  // GET DATA FROM DATABASE
+  useEffect(() => {
+    Axios.get("http://localhost:8000/item", {
+      params: {
+        user_id: localStorage.getItem("id"),
+      },
+    })
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // HANDLE ADD ITEM
   const addItem = (item) => {
-    console.log(item);
+    if (!item.name || /^\s*$/.test(item.username)) {
+      return;
+    }
+
+    Axios.post("http://localhost:8000/item/create", item);
+
+    const newItems = [item, ...items];
+    setItems(newItems);
   };
 
   return (
-    <div>
-      <Item />
-      <Item />
-      <Item />
-      <Item />
+    <Container>
+      <Row>
+        <Item items={items} />
+      </Row>
       <ItemForm onSubmit={addItem} show={props.show} hide={props.hide} />
-    </div>
+    </Container>
   );
 }
