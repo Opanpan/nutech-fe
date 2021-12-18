5fffbb39d572bc16fb284d83fc4a42b1a179b7a9
Opanpan <fanalriansyah@gmail.com>
2021-12-19 03:05:11 +0700

    change data item from props

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,20 +3,34 @@ import UseAnimations from "react-useanimations";
 import Edit from "react-useanimations/lib/edit";
 import Delete from "react-useanimations/lib/trash";
 
-const Item = () => {
-  return (
-    <Col>
-      <div className="item-container d-flex align-items-center justify-content-around mt-5">
-        <div className="item-image"></div>
-        <div className="item-font">Pakaian Anak</div>
-        <div className="item-font">Rp. 15,000</div>
-        <div className="item-font">Rp. 20,000</div>
-        <div className="item-font">20</div>
-        <UseAnimations animation={Edit} />
-        <UseAnimations animation={Delete} />
-      </div>
-    </Col>
-  );
+const Item = ({ items }) => {
+  return items.map((item, index) => {
+    return (
+      <Col key={index}>
+        <div className="item-container d-flex align-items-center justify-content-around mt-5">
+          <div className="item-image"></div>
+          <div className="item-font">
+            <h4 className="item-title">Nama</h4>
+            {item.name}
+          </div>
+          <div className="item-font">
+            <h4 className="item-title">Harga Beli</h4>
+            Rp {item.purchase_price}
+          </div>
+          <div className="item-font">
+            <h4 className="item-title">Harga Jual</h4>
+            Rp {item.selling_price}
+          </div>
+          <div className="item-font">
+            <h4 className="item-title">Stok</h4>
+            {item.quantity} / Pcs
+          </div>
+          <UseAnimations animation={Edit} />
+          <UseAnimations animation={Delete} />
+        </div>
+      </Col>
+    );
+  });
 };
 
 export default Item;
