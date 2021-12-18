602e0b07ecaf063f5f25fec0e88c75b94140220b
Opanpan <fanalriansyah@gmail.com>
2021-12-19 03:03:20 +0700

    set user and id to local storage

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,7 @@ const TOKEN_KEY = "jwt";
 export const login = (data) => {
   localStorage.setItem(TOKEN_KEY, data.token);
   localStorage.setItem("user", data.user.username);
+  localStorage.setItem("id", data.user.id);
 };
 
 export const logout = () => {
